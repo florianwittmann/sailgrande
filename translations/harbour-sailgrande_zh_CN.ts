bfd7b78b631f26a9cdce79a825feb85fcc5b5df1
TylerTemp <tylertempdev@gmail.com>
2015-03-25 21:27:51 +0800

    add Chinese Simplified (zh_CN) translation

---
+++ translations/harbour-sailgrande_zh_CN.ts
@@ -0,0 +1,362 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="17"/>
+        <source>About</source>
+        <translation type="unfinished">关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="36"/>
+        <source>An unofficial client for Instagram.</source>
+        <translation type="unfinished">Instagram的非官方客户端。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="54"/>
+        <source>developer</source>
+        <translation type="unfinished">开发者</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="115"/>
+        <source>Write a mail</source>
+        <translation type="unfinished">发送邮件</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="122"/>
+        <source>License</source>
+        <translation type="unfinished">许可协议</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="126"/>
+        <source>Source code is licensed under the MIT License (MIT).</source>
+        <translation type="unfinished">源代码基于MIT协议（MIT）</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="135"/>
+        <source>Contribute</source>
+        <translation type="unfinished">贡献者</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="148"/>
+        <source>Translate</source>
+        <translation type="unfinished">翻译</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="155"/>
+        <source>Report bugs</source>
+        <translation type="unfinished">反馈BUG</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="161"/>
+        <source>donating = loving</source>
+        <translation type="unfinished">捐助 = 爱</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="175"/>
+        <source>Paypal EUR</source>
+        <translation type="unfinished">Paypal 欧元</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="182"/>
+        <source>Paypal USD</source>
+        <translation type="unfinished">Paypal 美元</translation>
+    </message>
+</context>
+<context>
+    <name>AuthPage</name>
+    <message>
+        <location filename="../qml/pages/AuthPage.qml" line="45"/>
+        <source>Welcome to SailGrande, an unoffical Instagram client for Sailfish. Please press &apos;continue&apos; to login to your Instagram account.</source>
+        <translation type="unfinished">欢迎来到SailGrande，一个非官方Instagram Sailfish客户端。请按下&apos;继续&apos;来登录你的Instagram账号。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AuthPage.qml" line="57"/>
+        <source>Continue</source>
+        <translation type="unfinished">继续</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorMessageLabel</name>
+    <message>
+        <location filename="../qml/components/ErrorMessageLabel.qml" line="8"/>
+        <source>An error occurred.</source>
+        <translation type="unfinished">发生了错误。</translation>
+    </message>
+</context>
+<context>
+    <name>MediaDetailPage</name>
+    <message>
+        <location filename="../qml/pages/MediaDetailPage.qml" line="23"/>
+        <source>Details</source>
+        <translation type="unfinished">细节</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaDetailPage.qml" line="37"/>
+        <source>likes</source>
+        <translation type="unfinished">喜欢</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaDetailPage.qml" line="37"/>
+        <source>comments</source>
+        <translation type="unfinished">评论</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaDetailPage.qml" line="37"/>
+        <source>You liked this.</source>
+        <translation type="unfinished">你已经喜欢过了。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaDetailPage.qml" line="189"/>
+        <source>Remove my like</source>
+        <translation type="unfinished">移除喜欢</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaDetailPage.qml" line="198"/>
+        <source>Like</source>
+        <translation type="unfinished">喜欢</translation>
+    </message>
+</context>
+<context>
+    <name>MediaStreamPage</name>
+    <message>
+        <location filename="../qml/pages/MediaStreamPage.qml" line="48"/>
+        <source>Pin this tag feed</source>
+        <translation type="unfinished">钉上这个标签订阅</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaStreamPage.qml" line="62"/>
+        <source>Refresh</source>
+        <translation type="unfinished">刷新</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaStreamPage.qml" line="70"/>
+        <source>Load more</source>
+        <translation type="unfinished">载入更多</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaStreamPage.qml" line="100"/>
+        <source>There is no picture in this feed.</source>
+        <translation type="unfinished">这个订阅没有任何图片。</translation>
+    </message>
+</context>
+<context>
+    <name>PinnedPage</name>
+    <message>
+        <location filename="../qml/pages/PinnedPage.qml" line="16"/>
+        <source>Pinned tags</source>
+        <translation type="unfinished">已钉上的标签。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PinnedPage.qml" line="57"/>
+        <source>Remove</source>
+        <translation type="unfinished">移除</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PinnedPage.qml" line="78"/>
+        <source>Set as favorite</source>
+        <translation type="unfinished">设为收藏</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="19"/>
+        <source>Settings</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="35"/>
+        <source>Startpage</source>
+        <translation type="unfinished">开始页</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="39"/>
+        <source>Show popular feed</source>
+        <translation type="unfinished">显示热门订阅</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="49"/>
+        <source>Customize the column and row count of the feed previews on the startpage:</source>
+        <translation type="unfinished">自定义开始页订阅预览的行列数：</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="60"/>
+        <source>Columns</source>
+        <translation type="unfinished">列</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="75"/>
+        <source>Rows</source>
+        <translation type="unfinished">行</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="91"/>
+        <source>Shows %1 items per feed preview.</source>
+        <translation type="unfinished">每个订阅预览显示%1个内容。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="101"/>
+        <source>Feeds</source>
+        <translation type="unfinished">订阅</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="105"/>
+        <source>Show user and date</source>
+        <translation type="unfinished">显示用户和日期</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="115"/>
+        <source>Show user and date inline</source>
+        <translation type="unfinished">单行显示用户和日期</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="124"/>
+        <source>Show captions</source>
+        <translation type="unfinished">显示题注</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="137"/>
+        <source>Logout</source>
+        <translation type="unfinished">注销</translation>
+    </message>
+</context>
+<context>
+    <name>StartPage</name>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="88"/>
+        <source>Welcome</source>
+        <translation type="unfinished">欢迎</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="127"/>
+        <source>My Feed</source>
+        <translation type="unfinished">我的订阅</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="134"/>
+        <source>Popular</source>
+        <translation type="unfinished">热门</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="142"/>
+        <source>Tagged with %1</source>
+        <translation type="unfinished">%1标签</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="176"/>
+        <source>All pinned tags</source>
+        <translation type="unfinished">所有钉上的标签</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="200"/>
+        <source>About</source>
+        <translation type="unfinished">关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="205"/>
+        <source>Settings</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="210"/>
+        <source>Search</source>
+        <translation type="unfinished">搜索</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="215"/>
+        <source>Refresh</source>
+        <translation type="unfinished">刷新</translation>
+    </message>
+</context>
+<context>
+    <name>TagSearchPage</name>
+    <message>
+        <location filename="../qml/pages/TagSearchPage.qml" line="22"/>
+        <source>Search for tag</source>
+        <translation type="unfinished">搜索标签</translation>
+    </message>
+</context>
+<context>
+    <name>UserDetailBlock</name>
+    <message>
+        <location filename="../qml/components/UserDetailBlock.qml" line="28"/>
+        <source>%1 posts</source>
+        <translation type="unfinished">%1个帖子</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/UserDetailBlock.qml" line="38"/>
+        <source>%1 followers</source>
+        <translation type="unfinished">%1个追随者</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/UserDetailBlock.qml" line="48"/>
+        <source>%1 following</source>
+        <translation type="unfinished">正追随%1的人</translation>
+    </message>
+</context>
+<context>
+    <name>UserListPage</name>
+    <message>
+        <location filename="../qml/pages/UserListPage.qml" line="43"/>
+        <source>Load more</source>
+        <translation type="unfinished">载入更多</translation>
+    </message>
+</context>
+<context>
+    <name>UserProfilPage</name>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="76"/>
+        <source>You follow %1</source>
+        <translation type="unfinished">你追随%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="78"/>
+        <source>You requested to follow %1</source>
+        <translation type="unfinished">你请求追随%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="98"/>
+        <source>%1 follows you</source>
+        <translation type="unfinished">%1追随你</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="100"/>
+        <source>%1 requested to follow you</source>
+        <translation type="unfinished">%1请求追随你</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="102"/>
+        <source>You blocked %1</source>
+        <translation type="unfinished">你拦截了%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="148"/>
+        <source>This profile is private.</source>
+        <translation type="unfinished">这份档案仅私人可见。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="234"/>
+        <location filename="../qml/pages/UserProfilPage.qml" line="236"/>
+        <source>Followers</source>
+        <translation type="unfinished">追随者。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="242"/>
+        <location filename="../qml/pages/UserProfilPage.qml" line="244"/>
+        <source>Following</source>
+        <translation type="unfinished">正在追随</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="249"/>
+        <source>Unfollow %1</source>
+        <translation type="unfinished">取消追随%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserProfilPage.qml" line="257"/>
+        <source>Follow %1</source>
+        <translation type="unfinished">追随%1</translation>
+    </message>
+</context>
+</TS>
